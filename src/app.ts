36354ce7d3985a5cd32e7242ff660753cd9fe534
six006 <six006@126.com>
2018-08-18 20:26:06 +0800

    使用装饰器

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-App({});
+import { Appify, BaseApp } from '@six006/core';
+
+@Appify()
+export default class extends BaseApp {
+
+}
--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,1 +1,21 @@
-Page({})
+import { Pageify, BasePage, BasePageData } from '@six006/core';
+
+interface PageData extends BasePageData {
+    name: string;
+}
+
+const defaultData: PageData = {
+    name: '杨剑峰'
+}
+
+@Pageify()
+export default class extends BasePage<PageData, any>{
+
+    constructor() {
+        super();
+    }
+
+    data = {
+        name: '杨剑峰'
+    }
+}
